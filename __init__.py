1ba37231adf83c8aebccd244ef136026e7a377c0
Abel Garcia <abelgarcia38348@gmail.com>
2024-04-02 17:58:24 -0600

    create project

---
+++ __init__.py
@@ -0,0 +1,16 @@
+
+from nodes import ImageCreationNotifier
+
+NODE_CLASS_MAPPINGS = {
+    "ImageCreationNotifier": ImageCreationNotifier,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "ImageCreationNotifier": "Image Creation Notifier",
+}
+
+__all__ = [
+    "NODE_CLASS_MAPPINGS",
+    "NODE_DISPLAY_NAME_MAPPINGS",
+    "ImageCreationNotifier",
+]
