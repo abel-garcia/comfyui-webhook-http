7efc5461797fabbfcd88e59e8ff799de1cff62ba
Abel Garcia <abelgarcia38348@gmail.com>
2024-04-05 09:35:45 -0600

    update: all logic in the custom node

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # comfyui-webhook-http
+
+## Introduction
+
+Image Save Notifier is a comfyui custom node designed to automate the process of saving images and notifying a specified endpoint upon image creation. This custom node is particularly useful for scenarios where images are generated dynamically and need to be saved to disk while simultaneously triggering a notification via a webhook.
--- __init__.py
+++ __init__.py
@@ -6,105 +6,100 @@ import json
 import base64
 from PIL import Image
 import numpy as np
+from comfy.cli_args import args
+from nodes import PngInfo
 
-class ImageCreationNotifier():
+class ImageSaveNotifier():
     """
     Image Creation Notifier sends an HTTP request to a specified endpoint
     once an image is created by Comfyui.
     """
     def __init__(self):
-        pass
+        self.output_dir = folder_paths.get_output_directory()
+        self.type = "output"
+        self.prefix_append = ""
+        self.compress_level = 4
     
     @classmethod
     def INPUT_TYPES(cls):
-        id_field = (
-            "STRING",
-            {
-                "default": "",
-                "placeholder": "distinct identifier to organize into output directory",
-            },
-        )
         return {
             "required": {
                 "images": ("IMAGE",),
+                "filename_prefix": ("STRING", {"default": "ComfyUI"}),
+                "webhook_url": ("STRING", {"default": "", "placeholder": "enter webhook URL"}),
             },
             "optional": {
-                "webhook_url": id_field,
-                "metadata": id_field,
+                "metadata": ("STRING", {"default": "", "placeholder": "enter metadata for webhook"}),
             },
+            "hidden": {"prompt": "PROMPT", "extra_pnginfo": "EXTRA_PNGINFO"},
         }
     
-    CATEGORY = "image/send_notification"
-    FUNCTION = "send_notification_webhook"
-    RETURN_TYPES = ("IMAGE",)
+    CATEGORY = "image/Save Image & Notify"
+    FUNCTION = "save_images_and_notify"
+    RETURN_TYPES = ()
+    OUTPUT_NODE = True
     
-    def send_notification_webhook(
-        self,
-        images,
-        webhook_url="",
-        metadata="",
-    ):
+    def save_images_and_notify(
+        self, 
+        images, 
+        filename_prefix="ComfyUI", 
+        webhook_url="", 
+        metadata="", 
+        prompt=None, 
+        extra_pnginfo=None,
+        ):
         
-        """
-        Sends a notification with the specified parameters to the webhook URL.
+        # Save Images 
+        filename_prefix += self.prefix_append
+        full_output_folder, filename, counter, subfolder, filename_prefix = folder_paths.get_save_image_path(filename_prefix, self.output_dir, images[0].shape[1], images[0].shape[0])
+        results = list()
+        for (batch_number, image) in enumerate(images):
+            i = 255. * image.cpu().numpy()
+            img = Image.fromarray(np.clip(i, 0, 255).astype(np.uint8))
+            metadata = None
+            if not args.disable_metadata:
+                metadata = PngInfo()
+                if prompt is not None:
+                    metadata.add_text("prompt", json.dumps(prompt))
+                if extra_pnginfo is not None:
+                    for x in extra_pnginfo:
+                        metadata.add_text(x, json.dumps(extra_pnginfo[x]))
 
-        Parameters:
-            images (list): A list of images to be processed and sent in the notification.
-            webhook_url (str, optional): The URL of the webhook where the notification will be sent.
-                Defaults to an empty string.
-            metadata (str, optional): Additional metadata to be included in the notification.
-                Defaults to an empty string.
-        """
-        
-        output_dir = (
-            folder_paths.get_temp_directory()
-        )
-        (
-            full_output_folder,
-            filename,
-            _,
-            _,
-            _,
-        ) = folder_paths.get_save_image_path("final", output_dir)
-        
-        single_file_path = os.path.join(full_output_folder, f"{filename}_.png")
-        single_image = 255.0 * images[0].cpu().numpy()
-        single_image_pil = Image.fromarray(single_image.astype(np.uint8))
-        single_image_pil.save(single_file_path)
-        
-        
-        # Open the file in binary mode and read its content before encoding to Base64
-        with open(single_file_path, "rb") as file:
-            file_content = file.read()
-            file_base64 = base64.b64encode(file_content).decode('utf-8')
-        
+            filename_with_batch_num = filename.replace("%batch_num%", str(batch_number))
+            file = f"{filename_with_batch_num}_{counter:05}_.png"
+            img.save(os.path.join(full_output_folder, file), pnginfo=metadata, compress_level=self.compress_level)
+            results.append({
+                "filename": file,
+                "subfolder": subfolder,
+                "type": self.type
+            })
+            counter += 1
+            
+        # Send Notification Via Webhook
         data = {
            "metadata": json.dumps(metadata),
-           "file_base64": file_base64,
+           "images": results,
         }
         
-        response = requests.post(
-            webhook_url,
-            data=data,
-        )
-        
-        if response.status_code == 204:
-            print("Successfully sent image.")
-        else:
+        response = requests.post(webhook_url, data=data)
+        if response.status_code > 300:
             print(f"Failed to send image. Status code: {response.status_code} - {response.text}")
-            
-        return (images,)
+        
+        
+        return { "ui": { "images": results } }
+    
+
 
 NODE_CLASS_MAPPINGS = {
-    "ImageCreationNotifier": ImageCreationNotifier,
+    "ImageSaveNotifier": ImageSaveNotifier,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "ImageCreationNotifier": "Image Creation Notifier",
+    "ImageSaveNotifier": "Image Save Notifier",
 }
 
 __all__ = [
     "NODE_CLASS_MAPPINGS",
     "NODE_DISPLAY_NAME_MAPPINGS",
-    "ImageCreationNotifier",
+    "ImageSaveNotifier",
 ]
